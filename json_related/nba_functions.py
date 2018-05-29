cef9e34e6c8f320135b7fb6e9a3bca23dcc9f70b
stibbs1998 <sj28tibb@siena.edu>
2018-05-29 08:01:47 -0400

    new x-y functions for velo, accel.

--- json_related/nba_functions.py
+++ json_related/nba_functions.py
@@ -8,7 +8,7 @@ def smoothing(a,n):
 
 
 
-def velo(data):
+def velo_accel(data):
 
     for game in data.keys():
 
@@ -19,10 +19,29 @@ def velo(data):
                 y = data[game][team][key]['y']
                 dx = np.array(x[2:]) - np.array(x[:-2])
                 dy = np.array(y[2:]) - np.array(y[:-2])
-                dist = np.sqrt(np.square(dx)+np.square(dy))
+               # dist = np.sqrt(np.square(dx)+np.square(dy))
+                dist = np.array(dx,dy)
                 velo = dist/0.08
                 data[game][team][key]['velo'] = velo
+                dv = velo[2:]-velo[:-2]
+                accel = dv/0.08
+                data[game][team][key]['accel'] = accel	
 
    
 
+def energy(data):	# W = F*d(cos(phi)) = $\delta$ KE
+	
+	for game in data.keys():
 
+		for team in ['homeplayers','awayplayers']:
+
+			for key in data[game][team].keys():
+				dx = np.array(x[2:]) - np.array(x[:-2])
+				dy = np.array(y[2:]) - np.array(y[:-2])
+				d = np.array(dx,dy)
+				mass = 0 # holder
+				acc = data[game][team][key]['accel']
+				force = np.multiply(mass,acc)
+				F = np.array(force,force)
+				Work = np.multiply(F,d)	
+				data[game][team][key]['work'] = Work
